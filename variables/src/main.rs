6fd0ea27adb6cae43f717a6e4da78c9a9cb27c70
mowemcfc <jcartermcfc@gmail.com>
2022-10-23 14:18:12 +1100

    add expressions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,15 +7,25 @@ fn main() {
 
     println!("Seconds in five hours: {FIVE_HOURS_IN_SECONDS}");
 
-    shadow();
+    shadow(13);
+    expression();
 }
 
-fn shadow() {
-    let x = 5;
+fn shadow(x: i32) {
     let x = x + 1;
     {
         let x = x * 2;
         println!("The value of x in the inner scope is: {x}");
     }
     println!("The value of x is: {x}");
+}
+
+
+fn expression() {
+    let y = {
+        let x = 1;
+        x + 1 // no semicolon implies this is an expression
+        // expressions don't have semicolons
+    };
+    println!("The value of y is: {y}");
 }
