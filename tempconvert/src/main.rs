f1b35da3ec8eb31ae0ccb5d4e0581ba309b305e8
mowemcfc <jcartermcfc@gmail.com>
2022-10-23 15:26:29 +1100

    finish tempconvert

--- tempconvert/src/main.rs
+++ tempconvert/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input your temperature in celcius");
+    loop {
+        let mut temp: String = String::new();
+        io::stdin()
+            .read_line(&mut temp)
+            .expect("Failed to read line");
+
+        let temp: f32 = match temp.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid temperature!");
+                continue;
+            }
+        };
+        println!("Entered temp: {temp} celcius");
+        let converted: f32 = convert(temp);
+        println!("Converted temp: {converted} fahrenheit")
+    }
+
+
+}
+
+fn convert(temp: f32) -> f32 {
+    return temp * 1.8 + 32.0;
 }
